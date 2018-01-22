98d2f94b134a2a2038952e17b5b1e5e53c7a7f8b
Jackie Peng <jackiepeng12@gmail.com>
2018-01-22 01:37:56 -0500

    Revert "Merge pull request #3 from loafyyy/origin/messaging"
    This reverts commit babeaf270ecb7967fb9e44c7b52e662a1a7d646e, reversing
    changes made to f6db3092829b654285264198544d128d5c0f5347.

--- app/src/main/java/com/example/jackie/graphtest/MainActivity.java
+++ app/src/main/java/com/example/jackie/graphtest/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.jackie.graphtest;
 
-import android.Manifest;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.telephony.SmsManager;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -20,10 +14,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.PointsGraphSeries;
@@ -41,24 +31,6 @@ public class MainActivity extends AppCompatActivity {
     private Button resetButton;
     private GraphView graph;
 
-<<<<<<< HEAD
-    EditText etMessage;
-    EditText etTelNr;
-    Button smsButton;
-
-
-    PointsGraphSeries<Point> series = new PointsGraphSeries<>();
-    List<Point> data = new LinkedList<>();
-
-    String errorMessage = "entry must be numeric";
-    Context mContext;
-    int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
-
-    String SENT = "SMS_SENT";
-    String DELIVERED = "SMS_DELIVERED";
-    PendingIntent sentPI, deliveredPI;
-    BroadcastReceiver smsSentReceiver, smsDeliveredReceiver;
-=======
     private PointsGraphSeries<Point> series = new PointsGraphSeries<>();
     private List<Point> data = new LinkedList<>();
 
@@ -67,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
 
     // high glucose level - user will get notification
     private double glucoseHigh = 130;
->>>>>>> origin/master
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mContext = this;
-        final Activity activity = (Activity) mContext;
 
         // find the views
         graph = (GraphView) findViewById(R.id.graph_view);
@@ -84,13 +54,6 @@ public class MainActivity extends AppCompatActivity {
         yEntry = (EditText) findViewById(R.id.y_entry);
         setCloseEditTextOnEnter(yEntry);
         addButton = (Button) findViewById(R.id.add_button);
-        smsButton = (Button) findViewById(R.id.sendSMS);
-
-        etMessage = (EditText) findViewById(R.id.etMessage);
-        etTelNr = (EditText) findViewById(R.id.etTelNr);
-
-        sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
-        deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
 
         // handle adding new data points
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -129,23 +92,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-        smsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String message = etMessage.getText().toString();
-                String telNr = etTelNr.getText().toString();
-
-                if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.SEND_SMS)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.SEND_SMS},
-                            MY_PERMISSIONS_REQUEST_SEND_SMS);
-                } else {
-                    SmsManager sms = SmsManager.getDefault();
-                    sms.sendTextMessage(telNr, null, message, sentPI, deliveredPI);
-                }
-            }
-        }) ;
 
         resetButton = (Button) findViewById(R.id.reset_button);
         resetButton.setOnClickListener(new View.OnClickListener() {
@@ -210,64 +156,6 @@ public class MainActivity extends AppCompatActivity {
         int mNotificationId = 1;
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(mNotificationId, mBuilder.build());
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        smsSentReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                switch (getResultCode()) {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(MainActivity.this, "SMS sent!", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                        Toast.makeText(MainActivity.this, "Generic failure!", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case SmsManager.RESULT_ERROR_NO_SERVICE:
-                        Toast.makeText(MainActivity.this, "No service!", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case SmsManager.RESULT_ERROR_NULL_PDU:
-                        Toast.makeText(MainActivity.this, "Null PDU!", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-                        Toast.makeText(MainActivity.this, "Radio off!", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        };
-
-        smsDeliveredReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                switch (getResultCode()) {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(MainActivity.this, "SMS delivered!", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case Activity.RESULT_CANCELED:
-                        Toast.makeText(MainActivity.this, "SMS not delivered!", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        };
-
-        registerReceiver(smsSentReceiver, new IntentFilter(SENT));
-        registerReceiver(smsDeliveredReceiver, new IntentFilter(DELIVERED));
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        unregisterReceiver(smsDeliveredReceiver);
-        unregisterReceiver(smsSentReceiver);
     }
 
     // allows keyboard to close when enter is pressed
